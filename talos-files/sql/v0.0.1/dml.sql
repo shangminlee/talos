1953a5d6f2e3b0c1aaeedc246d684f3283e2c981
shangminlee <shangmlee@foxmail.com>
2017-07-08 11:32:27 +0800

    表结构初始化

---
+++ talos-files/sql/v0.0.1/dml.sql
@@ -0,0 +1,56 @@
+#权限路径表
+DROP TABLE IF EXISTS `talos_url_permission`;
+CREATE TABLE `talos_url_permission` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `url` varchar(256) DEFAULT NULL COMMENT 'url地址',
+  `description` varchar(64) DEFAULT NULL COMMENT 'url描述',
+  `m_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `c_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='权限路径表';
+
+#角色表
+DROP TABLE IF EXISTS `talos_role`;
+CREATE TABLE `talos_role` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `name` varchar(32) DEFAULT NULL COMMENT '角色名称',
+  `type` varchar(10) DEFAULT NULL COMMENT '角色类型',
+  `m_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `c_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT='角色表';
+
+#用户角色权限表
+DROP TABLE IF EXISTS `talos_role_permission`;
+CREATE TABLE `talos_role_permission` (
+  `rid` bigint(20) DEFAULT NULL COMMENT '角色ID',
+  `pid` bigint(20) DEFAULT NULL COMMENT '权限ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT='用户角色权限表';
+
+#用户信息表
+DROP TABLE IF EXISTS `talos_user`;
+CREATE TABLE `talos_user` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
+  `mobile` VARCHAR(50) NOT NULL COMMENT '手机号',
+  `nickname` varchar(20) DEFAULT NULL COMMENT '用户昵称',
+  `email` varchar(128) DEFAULT NULL COMMENT '邮箱|登录帐号',
+  `pswd` varchar(32) DEFAULT NULL COMMENT '密码',
+  `last_login_time` datetime DEFAULT NULL COMMENT '最后登录时间',
+  `status` bigint(1) DEFAULT '1' COMMENT '1:有效，0:禁止登录',
+  `m_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '修改时间',
+  `c_time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COMMENT = '用户信息表';
+
+#用户角色表
+DROP TABLE IF EXISTS `talos_user_role`;
+CREATE TABLE `talos_user_role` (
+  `uid` bigint(20) DEFAULT NULL COMMENT '用户ID',
+  `rid` bigint(20) DEFAULT NULL COMMENT '角色ID'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COMMENT = '用户信息表';
+
+
+
+
+
+
